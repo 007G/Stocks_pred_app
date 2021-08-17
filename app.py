1b065af87ebd47d6a5b66a4a1fadcb00cd3b0af0
GAURAV SINGH <gaurav7jpr@gmail.com>
2021-08-18 03:33:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import datetime
 from datetime import date
 
 import yfinance as yf
@@ -6,61 +7,71 @@ from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
+import pandas as pd
+import numpy as np
 
-st.title('Stock Forecast App')
-
-stocks = ('GOOG', 'AAPL', 'MSFT', 'FB','CSCO','QCOM','SBUX','TSLA')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
-
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
+import math
 
 
-@st.cache(allow_output_mutation=True)
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
+def main():
+    START = "2015-01-01"
+    TODAY = date.today().strftime('%Y-%m-%d')
+
+    st.title("Forecasting App")
+    html_temp = """
+        <div style="background-color:tomato;padding:10px">
+        <h2 style="color:white;text-align:center;">This is a Stock Price Forecasting Application </h2>
+        </div>
+        """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    stocks = ('GOOG', 'AAPL', 'MSFT', 'FB','CSCO','QCOM','SBUX','TSLA')
+    selected_stock = st.selectbox("Choose a stock for prediction: ", stocks)
+    n_years = st.slider('Years of prediction:', 1, 4)
+    period = number_years * 365
+
+    data_load_State = st.text("Load data.....")
+
+    @st.cache(allow_output_mutation=True)
+    def loading_data(ticker):
+        df1 = yf.download(ticker, START, TODAY)
+        df1.reset_index(inplace=True)
+        return df1
+    df1 = loading_data(selected_stock)
+    df1["Date"]=pd.to_datetime(df1['Date']).dt.date
+    data_load_State.text("Loading data.......Done")
+
+    st.subheader("Raw data")
+    st.write(df1.tail())
+    def plot_raw_data():
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=df1['Date'], y=df1["Open"], name="Stock_Open"))
+        fig.add_trace(go.Scatter(x=df1['Date'], y=df1["Close"], name="Stock_Close"))
+        fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+    plot_raw_data()
 
 
-data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
-data_load_state.text('Loading data... done!')
 
-st.subheader('Raw data')
-st.write(data.tail())
+    df_train = df1[["Date","Close"]]
+    df_train =df_train.rename(columns={"Date":"ds","Close":"y"})
+    model = Prophet()
+    model.fit(df_train)
+    pred = model.make_future_dataframe(periods=period)
+    forecast = model.predict(pred)
+
+    st.write("Forecsted Data")
+    def plot_final_data():
+        fig1 = plot_plotly(model,forecast)
+        st.plotly_chart(fig1)
+    plot_final_data()
+
+    st.write("Forecast Componet")
+    fig2 = model.plot_components(forecast)
+    st.write(fig2)
 
 
-# Plot raw data
-def plot_raw_data():
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
+# driver code
+if __name__ == '__main__':
+    main()
 
-
-plot_raw_data()
-
-# Predict forecast with Prophet.
-df_train = data[['Date', 'Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
-
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
-
-# Show and plot forecast
-st.subheader('Forecast data')
-st.write(forecast.tail())
-
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
-
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
